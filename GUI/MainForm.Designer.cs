5a94d4b52c7c75197dd45703cea17ab57f60333b
Margarita Wraith <Kondratieff@live.ru>
2018-01-31 14:40:26 +0300

    Добавлено получение сигнала по спектру, обновлена форма, класс спектра вынесен в отдельный файл.

--- GUI/MainForm.Designer.cs
+++ GUI/MainForm.Designer.cs
@@ -31,16 +31,20 @@
             this.MainMenu = new System.Windows.Forms.MenuStrip();
             this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.signalFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spectrumFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuSave = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
             this.SignalList = new System.Windows.Forms.ListBox();
-            this.SpectrList = new System.Windows.Forms.ListBox();
+            this.SpectrumList = new System.Windows.Forms.ListBox();
             this.ComputeButton = new System.Windows.Forms.Button();
             this.dtLabel = new System.Windows.Forms.Label();
             this.dfLabel = new System.Windows.Forms.Label();
             this.Signai_GroupBox = new System.Windows.Forms.GroupBox();
+            this.Spectrum_GroupBox = new System.Windows.Forms.GroupBox();
             this.MainMenu.SuspendLayout();
             this.Signai_GroupBox.SuspendLayout();
+            this.Spectrum_GroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainMenu
@@ -51,7 +55,7 @@
             this.MainMenu.Location = new System.Drawing.Point(0, 0);
             this.MainMenu.Name = "MainMenu";
             this.MainMenu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.MainMenu.Size = new System.Drawing.Size(755, 24);
+            this.MainMenu.Size = new System.Drawing.Size(754, 24);
             this.MainMenu.TabIndex = 0;
             this.MainMenu.Text = "menuStrip1";
             // 
@@ -67,53 +71,70 @@
             // 
             // MenuOpen
             // 
+            this.MenuOpen.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.signalFileToolStripMenuItem,
+            this.spectrumFileToolStripMenuItem});
             this.MenuOpen.Name = "MenuOpen";
-            this.MenuOpen.Size = new System.Drawing.Size(103, 22);
+            this.MenuOpen.Size = new System.Drawing.Size(152, 22);
             this.MenuOpen.Text = "Open";
-            this.MenuOpen.Click += new System.EventHandler(this.MenuOpen_Click);
+            // 
+            // signalFileToolStripMenuItem
+            // 
+            this.signalFileToolStripMenuItem.Name = "signalFileToolStripMenuItem";
+            this.signalFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.signalFileToolStripMenuItem.Text = "Signal File";
+            this.signalFileToolStripMenuItem.Click += new System.EventHandler(this.signalFileToolStripMenuItem_Click);
+            // 
+            // spectrumFileToolStripMenuItem
+            // 
+            this.spectrumFileToolStripMenuItem.Name = "spectrumFileToolStripMenuItem";
+            this.spectrumFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.spectrumFileToolStripMenuItem.Text = "Spectrum File";
+            this.spectrumFileToolStripMenuItem.Click += new System.EventHandler(this.spectrumFileToolStripMenuItem_Click);
             // 
             // MenuSave
             // 
             this.MenuSave.Name = "MenuSave";
-            this.MenuSave.Size = new System.Drawing.Size(103, 22);
+            this.MenuSave.Size = new System.Drawing.Size(152, 22);
             this.MenuSave.Text = "Save";
             // 
             // MenuExit
             // 
             this.MenuExit.Name = "MenuExit";
-            this.MenuExit.Size = new System.Drawing.Size(103, 22);
+            this.MenuExit.Size = new System.Drawing.Size(152, 22);
             this.MenuExit.Text = "Exit";
             this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
             // 
             // SignalList
             // 
-            this.SignalList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SignalList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.SignalList.FormattingEnabled = true;
-            this.SignalList.Location = new System.Drawing.Point(2, 15);
-            this.SignalList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SignalList.Location = new System.Drawing.Point(3, 17);
+            this.SignalList.Margin = new System.Windows.Forms.Padding(2);
             this.SignalList.Name = "SignalList";
-            this.SignalList.Size = new System.Drawing.Size(302, 476);
+            this.SignalList.Size = new System.Drawing.Size(300, 407);
             this.SignalList.TabIndex = 1;
             // 
-            // SpectrList
+            // SpectrumList
             // 
-            this.SpectrList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.SpectrumList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.SpectrList.FormattingEnabled = true;
-            this.SpectrList.Location = new System.Drawing.Point(469, 25);
-            this.SpectrList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.SpectrList.Name = "SpectrList";
-            this.SpectrList.Size = new System.Drawing.Size(278, 472);
-            this.SpectrList.TabIndex = 1;
+            this.SpectrumList.FormattingEnabled = true;
+            this.SpectrumList.Location = new System.Drawing.Point(5, 15);
+            this.SpectrumList.Margin = new System.Windows.Forms.Padding(2);
+            this.SpectrumList.Name = "SpectrumList";
+            this.SpectrumList.Size = new System.Drawing.Size(287, 407);
+            this.SpectrumList.TabIndex = 1;
             // 
             // ComputeButton
             // 
             this.ComputeButton.Location = new System.Drawing.Point(322, 25);
-            this.ComputeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ComputeButton.Margin = new System.Windows.Forms.Padding(2);
             this.ComputeButton.Name = "ComputeButton";
             this.ComputeButton.Size = new System.Drawing.Size(117, 48);
             this.ComputeButton.TabIndex = 2;
-            this.ComputeButton.Text = "Compute Spectr";
+            this.ComputeButton.Text = "Compute";
             this.ComputeButton.UseVisualStyleBackColor = true;
             this.ComputeButton.Click += new System.EventHandler(this.ComputeButton_Click);
             // 
@@ -139,34 +160,49 @@
             // 
             // Signai_GroupBox
             // 
+            this.Signai_GroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.Signai_GroupBox.Controls.Add(this.SignalList);
-            this.Signai_GroupBox.Location = new System.Drawing.Point(9, 25);
-            this.Signai_GroupBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Signai_GroupBox.Location = new System.Drawing.Point(11, 25);
+            this.Signai_GroupBox.Margin = new System.Windows.Forms.Padding(2);
             this.Signai_GroupBox.Name = "Signai_GroupBox";
-            this.Signai_GroupBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Signai_GroupBox.Size = new System.Drawing.Size(306, 493);
+            this.Signai_GroupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.Signai_GroupBox.Size = new System.Drawing.Size(307, 433);
             this.Signai_GroupBox.TabIndex = 5;
             this.Signai_GroupBox.TabStop = false;
-            this.Signai_GroupBox.Text = "Сигнал";
+            this.Signai_GroupBox.Text = "Signal";
+            // 
+            // Spectrum_GroupBox
+            // 
+            this.Spectrum_GroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Spectrum_GroupBox.Controls.Add(this.SpectrumList);
+            this.Spectrum_GroupBox.Location = new System.Drawing.Point(445, 27);
+            this.Spectrum_GroupBox.Name = "Spectrum_GroupBox";
+            this.Spectrum_GroupBox.Size = new System.Drawing.Size(297, 430);
+            this.Spectrum_GroupBox.TabIndex = 6;
+            this.Spectrum_GroupBox.TabStop = false;
+            this.Spectrum_GroupBox.Text = "Spectrum";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 528);
+            this.ClientSize = new System.Drawing.Size(754, 469);
+            this.Controls.Add(this.Spectrum_GroupBox);
             this.Controls.Add(this.Signai_GroupBox);
             this.Controls.Add(this.dfLabel);
             this.Controls.Add(this.dtLabel);
             this.Controls.Add(this.ComputeButton);
-            this.Controls.Add(this.SpectrList);
             this.Controls.Add(this.MainMenu);
             this.MainMenuStrip = this.MainMenu;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "Главное окно приложения";
             this.MainMenu.ResumeLayout(false);
             this.MainMenu.PerformLayout();
             this.Signai_GroupBox.ResumeLayout(false);
+            this.Spectrum_GroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,11 +216,14 @@
         private System.Windows.Forms.ToolStripMenuItem MenuSave;
         private System.Windows.Forms.ToolStripMenuItem MenuExit;
         private System.Windows.Forms.ListBox SignalList;
-        private System.Windows.Forms.ListBox SpectrList;
+        private System.Windows.Forms.ListBox SpectrumList;
         private System.Windows.Forms.Button ComputeButton;
         private System.Windows.Forms.Label dtLabel;
         private System.Windows.Forms.Label dfLabel;
         private System.Windows.Forms.GroupBox Signai_GroupBox;
+        private System.Windows.Forms.ToolStripMenuItem signalFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spectrumFileToolStripMenuItem;
+        private System.Windows.Forms.GroupBox Spectrum_GroupBox;
     }
 }
 
--- GUI/MainForm.cs
+++ GUI/MainForm.cs
@@ -14,6 +14,7 @@ namespace GUI
     public partial class MainForm : Form
     {
         private DigitalSignal f_Signal;
+        private DigitalSpectrum f_Spectrum;
         public MainForm()
         {
             InitializeComponent();
@@ -48,17 +49,75 @@ namespace GUI
 
         private void ComputeButton_Click(object sender, EventArgs e)
         {
-            if (f_Signal == null)
+            if (f_Signal == null && f_Spectrum == null)
             {
                 MessageBox.Show("No signal loaded", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            var Spectrum = f_Signal.GetSpectrum();
-            dfLabel.Text = string.Format("df={0} Hz", Spectrum.df.ToString());
-            SpectrList.Items.Clear();
-            for (int i = 0; i < Spectrum.Length; i++)
+            if (f_Signal != null)
             {
-                SpectrList.Items.Add(Spectrum[i]);
+                var Spectrum = f_Signal.GetSpectrum();
+                dfLabel.Text = string.Format("df={0} Hz", Spectrum.df.ToString());
+                SpectrumList.Items.Clear();
+                for (int i = 0; i < Spectrum.Length; i++)
+                {
+                    SpectrumList.Items.Add(Spectrum[i]);
+                }
+            }
+            if (f_Spectrum != null)
+            {
+                var Signal = f_Spectrum.GetSignal();
+                dtLabel.Text = string.Format("dt={0} s", Signal.dt.ToString());
+                SignalList.Items.Clear();
+                for (int i = 0; i < Signal.Length; i++)
+                {
+                    SignalList.Items.Add(Signal[i]);
+                }
+            }
+
+
+        }
+
+        private void signalFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Выберите файл сигнала";
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "Text(*.txt)|*.txt";
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var file_name = dialog.FileName;
+                var s = DigitalSignal.LoadFromFile(file_name);
+                f_Signal = s;
+                dtLabel.Text = string.Format("dt={0} s", s.dt.ToString());
+                SignalList.Items.Clear();
+                for (int i = 0; i < s.Length; i++)
+                {
+                    SignalList.Items.Add(s[i]);
+                }
+
+            }
+        }
+
+        private void spectrumFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Выберите файл спектра";
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "Text(*.txt)|*.txt";
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var file_name = dialog.FileName;
+                var S = DigitalSpectrum.LoadFromFile(file_name);
+                f_Spectrum = S;
+                dfLabel.Text = string.Format("df={0} Hz", S.df.ToString());
+                SpectrumList.Items.Clear();
+                for (int i = 0; i < S.Length; i++)
+                {
+                    SpectrumList.Items.Add(S[i]);
+                }
             }
         }
     }
